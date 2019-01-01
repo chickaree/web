29c8957d456fd11d5223934ebfc9620b8ea8ee9d
David Barratt <david@davidwbarratt.com>
2019-01-01 16:06:45 -0500

    Style feed list

--- pages/query.js
+++ pages/query.js
@@ -69,16 +69,108 @@ const Icon = ({ src, alt }) => {
   }
 
   return (
-    <div className="col-4 col-lg-2">
-      <img src={src} alt={alt} className="icon w-100 img-thumbnail" />
+    <div className="embed-responsive embed-responsive-1by1">
+        <div className="embed-responsive-item border rounded p-1">
+          <div className="row align-items-center h-100">
+            <div className="col">
+              <img src={src} alt={alt} className="w-100" />
+            </div>
+          </div>
+        </div>
     </div>
   );
 };
 
-const Query = ({ data, sitename, description, banner, icon, feeds }) => {
+const WebsiteIcon = ({ src, alt }) => {
+  if (!src) {
+    return null;
+  }
+
+  return (
+    <div className="col-4 col-lg-2">
+      <div className="icon">
+        <Icon src={src} alt={alt} />
+      </div>
+    </div>
+  );
+};
+
+const FeedIcon = ({ href, src, alt }) => {
+  if (!src) {
+    return null;
+  }
+
+  return (
+    <Link {...getLinkData(href)}>
+      <a className="d-block">
+        <Icon src={src} alt={alt} />
+      </a>
+    </Link>
+  );
+};
+
+const WebsiteDescription = ({ description }) => {
+  if (!description) {
+    return null;
+  }
+
+  return (
+    <div className="col-12 col-lg">
+      <p>{description}</p>
+    </div>
+  );
+}
+
+const FeedDescription = ({ description }) => {
+  if (!description) {
+    return null;
+  }
+
+  return (
+    <p>{description}</p>
+  );
+}
+
+const FeedList = ({ feeds, hasIcon }) => {
+  if (!feeds || !feeds.length) {
+    return null;
+  }
+
+  const hasFeedIcons = !!feeds.filter(feed => !!feed.icon).length;
+
+  return (
+    <div className="row mb-3">
+      <div className={hasIcon ? 'col-lg-10 offset-lg-2 col' : 'col'}>
+        <div className="card">
+          <ol className="list-group list-group-flush">
+            {feeds.map(feed => (
+              <li className="list-group-item" key={feed.feed_url}>
+                <div className="row">
+                  <div className={hasFeedIcons ? 'col-3 col-md-2' : ''}>
+                    <FeedIcon href={feed.feed_url} src={feed.icon} alt={feed.title} />
+                  </div>
+                  <div className="col">
+                    <h5>
+                      <Link {...getLinkData(feed.feed_url)}><a>{feed.title}</a></Link>
+                    </h5>
+                    <FeedDescription description={feed.description} />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Query = ({ sitename, description, banner, icon, feeds }) => {
   let className = [
     'container',
   ];
+
+
 
   if (banner) {
     className = [
@@ -92,24 +184,17 @@ const Query = ({ data, sitename, description, banner, icon, feeds }) => {
       <Banner src={banner} alt={sitename} />
       <div className={className.join(' ')}>
         <div className="row mt-3">
-          <Icon src={icon} alt={sitename} />
-          <div className="col-auto">
-            <h2>{sitename}</h2>
-          </div>
-          <div className="col-12 col-lg">
-            <p>{description}</p>
-          </div>
-        </div>
-        <div className="row">
+          <WebsiteIcon src={icon} alt={sitename} />
           <div className="col">
-            {feeds.map(feed => (
-              <div key={feed.href}>
-                <Link {...getLinkData(feed.href)}>{feed.title || 'No Title'}</Link> - {feed.href}
+            <div className="row">
+              <div className="col-12 col-lg-auto">
+                <h2>{sitename}</h2>
               </div>
-            ))}
-            <code style={{overflowWrap: 'break-word'}}>{data}</code>
+              <WebsiteDescription description={description} />
+            </div>
           </div>
         </div>
+        <FeedList feeds={feeds} hasIcon={!!icon} />
       </div>
     </Layout>
   );
@@ -170,7 +255,7 @@ Query.getInitialProps = async ({ query, res }) => {
 
     return bSize - aSize;
   });
-  const feeds = $('link[rel="alternate"]').toArray().map((link, index) => ({
+  const feeds = await Promise.all($('link[rel="alternate"]').toArray().map((link, index) => ({
     ...link.attribs,
     order: index,
   })).filter((feed) => {
@@ -217,10 +302,55 @@ Query.getInitialProps = async ({ query, res }) => {
   }, []).sort((a, b) => a.order - b.order).map(feed => ({
     ...feed,
     href: feed.href ? new URL(feed.href, response.url).href : null,
+  })).map(async (feed) => {
+    const response = await fetch(feed.href);
+
+    if (response.headers.has('Content-Type') && response.headers.get('Content-Type').includes('application/json')) {
+      const data = await response.json();
+      return {
+        title: data.title || '',
+        icon: data.icon,
+        feed_url: response.url,
+        description: data.description || '',
+      };
+    }
+
+    const data = await response.text();
+    const $ = cherrio.load(data, {
+      xmlMode: true,
+    });
+
+    let root = $.root().children().first();
+    if (root.is('rss')) {
+      root = root.children().first();
+    }
+
+    if (root.is('channel')) {
+      return {
+        title: $('> title', root).last().text(),
+        feed_url: response.url,
+        icon: $('> image > url', root).last().text(),
+        description: $('> description', root).last().text(),
+      }
+    }
+
+    if (root.is('feed')) {
+      return {
+        title: $('> title', root).last().text(),
+        feed_url: response.url,
+        icon: $('> icon', root).last().text(),
+        description: $('> description', root).last().text(),
+      }
+    }
+
+    return {
+      title: feed.title || '',
+      feed_url: response.url,
+      description: '',
+    };
   }));
 
   return {
-    data,
     sitename,
     description,
     banner: banner ? new URL(banner, response.url).href : null,
