ae5e86c01956188f5e7136fcda6fc6d2cd51cccf
David Barratt <david@davidwbarratt.com>
2020-05-06 22:23:05 -0400

    Simplify cloudflare worker

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-import routes from './.next/routes-manifest.json';
+import { getEventHandler } from '@cinematix/next-cloudflare';
+import routesManifest from './.next/routes-manifest.json';
 
-async function getNotFoundResponse(url, event) {
-  const notFoundResponse = await getAssetFromKV(event, {
-    mapRequestToAsset: (req) => new Request((new URL('/404.html', url.origin)).toString(), req),
-  });
-
-  return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
-}
-
-async function handleEvent(event) {
-  const url = new URL(event.request.url);
-
-  try {
-    return await getAssetFromKV(event);
-  } catch (e) {
-    const match = routes.dynamicRoutes.find(({ regex }) => (url.pathname.match(regex)));
-
-    if (match) {
-      try {
-        return await getAssetFromKV(event, {
-          mapRequestToAsset: (req) => new Request((new URL(`${match.page}.html`, url.origin)).toString(), req),
-        });
-      } catch (error) {
-        return getNotFoundResponse(url, event);
-      }
-    }
-
-    return getNotFoundResponse(url, event);
-  }
-}
+const handleEvent = getEventHandler(routesManifest);
 
 // eslint-disable-next-line no-restricted-globals
-addEventListener('fetch', (event) => {
-  event.respondWith(handleEvent(event));
-});
+addEventListener('fetch', handleEvent);
