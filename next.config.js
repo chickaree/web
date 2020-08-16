9215679b870a30533d494364f78cda9d4731374b
David Barratt <david@davidwbarratt.com>
2020-08-16 14:48:01 -0400

    Fix asset paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const { InjectManifest } = require('workbox-webpack-plugin');
 const { join } = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   env: {
@@ -13,19 +14,32 @@ module.exports = {
     });
 
     if (!isServer && !dev) {
+      // Copy the public files so they are picked up by the manifest.
+      config.plugins.push(new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: 'public',
+          },
+        ],
+      }));
+
       config.plugins.push(new InjectManifest({
         swSrc: join(__dirname, 'sw.js'),
         swDest: join(__dirname, 'public', 'sw.js'),
         exclude: [
           'react-loadable-manifest.json',
           'build-manifest.json',
+          'public/sw.js',
           /\.map$/,
         ],
         modifyURLPrefix: {
           'static/': '_next/static/',
+          'public/': '/',
         },
       }));
     }
+
     return config;
   },
 };
